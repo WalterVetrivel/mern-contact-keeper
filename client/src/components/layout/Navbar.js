7ae4a7e5cc47d63bba75522ee3a3f63c0e9bbb59
Walter Vetrivel <waltervetrivel@yahoo.in>
2019-10-15 17:11:52 +1100

    Added navbar to React front-end

---
+++ client/src/components/layout/Navbar.js
@@ -0,0 +1,38 @@
+import React from 'react';
+import {Link} from 'react-router-dom';
+import PropTypes from 'prop-types';
+
+import {faIdCardAlt} from '@fortawesome/free-solid-svg-icons';
+import {FontAwesomeIcon} from '@fortawesome/react-fontawesome';
+
+const Navbar = ({title, icon}) => {
+	return (
+		<div className="navbar bg-primary">
+			<h1>
+				<FontAwesomeIcon icon={icon} />
+				&nbsp;
+				{title}
+			</h1>
+			<ul>
+				<li>
+					<Link to="/">Home</Link>
+				</li>
+				<li>
+					<Link to="/about">About</Link>
+				</li>
+			</ul>
+		</div>
+	);
+};
+
+Navbar.propTypes = {
+	title: PropTypes.string.isRequired,
+	icon: PropTypes.object
+};
+
+Navbar.defaultProps = {
+	title: 'Contact Keeper',
+	icon: faIdCardAlt
+};
+
+export default Navbar;
